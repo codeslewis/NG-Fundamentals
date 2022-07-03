3ca6e1b917df8e935396351302f1debc6c41e720
Lewis Sneddon <lsneddon@actify.com>
2022-07-03 08:28:52 +0100

    Routing - Accessing Route Parameters

--- src/app/events/event-details/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {EventService} from "../../shared/event.service";
+import { EventService } from "../../shared/event.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -9,10 +10,13 @@ export class EventDetailsComponent implements OnInit {
 
     public event: any;
 
-    constructor(private eventService: EventService) { }
+    constructor(
+        private eventService: EventService,
+        private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        this.event = this.eventService.getEvent(1);
+        const eventId: number = parseInt(this.route.snapshot.params['id']);
+        this.event = this.eventService.getEvent(eventId);
     }
 
 }
