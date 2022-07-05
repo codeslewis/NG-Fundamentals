a2b284b8f79db15fe0820248bc0c75386e70b709
Lewis Sneddon <lsneddon@actify.com>
2022-07-05 16:39:00 +0100

    Collecting Data - Two way data binding, banana in a box

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
-import { EventService } from "../shared";
+import { EventService, IEvent } from "../shared";
 
 @Component({
   selector: 'create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.css']
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit{
 
     private _isDirty: boolean = true;
 
-    public newEvent: any;
+    public event: IEvent;
 
     constructor(
         private router: Router,
         private eventService: EventService) { }
+
+    ngOnInit(): void {
+        this.event = {
+            id: 999,
+            name: 'NG Spectacular',
+            date: new Date('8/8/2008'),
+            time: '10am',
+            price: 799.99,
+            location: {
+                address: '456 Happy Street',
+                city: 'Felicity',
+                country: 'Angularisation'
+            },
+            sessions: [],
+            onlineUrl: 'http://ngSpectacular.com',
+            imageUrl: 'http://ngSpectacular.com/logo.png'
+        }
+    }
 
     public saveEvent(formValues: any): void {
         console.log(formValues);
