08be842d417e5663dfe8ea383a235e1da68bdf14
Lewis Sneddon <lsneddon@actify.com>
2022-07-06 09:06:40 +0100

    Collecting Data - Implemented noop for null in custom validator

--- src/app/events/shared/restricted-words.validator.ts
+++ src/app/events/shared/restricted-words.validator.ts
@@ -1,6 +1,10 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function restrictedWords(words: string[]): ValidatorFn {
+    if (words == null) {
+        return (_: AbstractControl) => null;
+    }
+
     return (control: AbstractControl): ValidationErrors | null => {
         let invalidWords: string[] = words
             .filter(word => control.value.includes(word));
